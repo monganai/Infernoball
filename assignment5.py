d4484eb1cdb393990e4d621cb0c33755186dad37
monganai <monganai@tcd.ie>
2018-11-01 19:03:36 +0000

    added assignment5.py - this program decrypts inferno.json using recoverd passwords from john.pot

---
+++ assignment5.py
@@ -0,0 +1,96 @@
+import json
+from pprint import pprint
+from hashlib import sha256
+import secretsharing as sss
+import base64
+from Crypto.Cipher import AES
+from Crypto import Random
+import hashlib
+
+def load_inferno():                             # loads a .json to a python dict 
+    with open('inferno.json') as f:
+        temp = json.load(f)
+   
+    return temp
+
+BLOCK_SIZE = 16
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
+unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+
+def decrypt(enc, password):
+    private_key = hashlib.sha256(password.encode("utf-8")).digest()
+    enc = base64.b64decode(enc)
+    iv = enc[:16]
+    cipher = AES.new(private_key, AES.MODE_CBC, iv)
+    return unpad(cipher.decrypt(enc[16:]))
+
+def pxor(pwd,share):
+    '''
+      XOR a hashed password into a Shamir-share
+
+      1st few chars of share are index, then "-" then hexdigits
+      we'll return the same index, then "-" then xor(hexdigits,sha256(pwd))
+      we truncate the sha256(pwd) to if the hexdigits are shorter
+      we left pad the sha256(pwd) with zeros if the hexdigits are longer
+      we left pad the output with zeros to the full length we xor'd
+    '''
+    words=share.split("-")
+    hexshare=words[1]
+    slen=len(hexshare)
+    hashpwd=sha256(pwd).hexdigest()
+    hlen=len(hashpwd)
+    outlen=0
+    if slen<hlen:
+        outlen=slen
+        hashpwd=hashpwd[0:outlen]
+    elif slen>hlen:
+        outlen=slen
+        hashpwd=hashpwd.zfill(outlen)
+    else:
+        outlen=hlen
+    xorvalue=int(hexshare, 16) ^ int(hashpwd, 16) # convert to integers and xor 
+    paddedresult='{:x}'.format(xorvalue)          # convert back to hex
+    paddedresult=paddedresult.zfill(outlen)       # pad left
+    result=words[0]+"-"+paddedresult              # put index back
+    return result
+
+
+
+inferno_Dict = load_inferno()
+hashes = inferno_Dict['hashes']
+shares = inferno_Dict['shares']
+pot = open('john.pot','r')
+#import pdb; pdb.set_trace()
+lines = pot.read().split('\n')
+xored = []      # list of plaintext xor share
+secrets = []
+
+for i, hash in enumerate(hashes):
+    for line in lines:
+     if hash in line:
+         pwd = line.split(':')[1]
+         xoring = pxor(pwd,shares[i])
+         xored.append(xoring)
+pprint(xored)
+
+
+
+
+#sss.SecretSharer.recover_secret(s[0:2])              get secret 
+#pprint(secret)
+#secrets.append(secret)                                      add to list of secrets 
+#get mode of secrets i guess 
+#decrypted = decrypt(encrypted, password)       pass it ciphertext / secret once passed k
+
+
+
+
+
+
+
+
+
+
+
+
+
